cc156dbeb9ad82a11ea3cad31f6a677036205e1c
manikanta <manikanta.kottakota322@gmail.com>
2020-08-09 22:27:50 +0530

    added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CareerComponent } from './career/career.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'aboutus',
+    component: AboutusComponent
+}, {
+    path: 'careers',
+    component: CareerComponent
+},{
+    path: 'contactus',
+    component: ContactComponent
+},{
+    path: 'home',
+    component: HomeComponent
+},{
+    path: 'services',
+    component: ServicesComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactComponent } from './contact/contact.component';
+import { CareerComponent } from './career/career.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactComponent,
+    CareerComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
