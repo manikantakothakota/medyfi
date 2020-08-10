7ac02f3cd1ae58a5884a16d4e2ffaf50f9b19139
manikanta <manikanta.kottakota322@gmail.com>
2020-08-10 10:12:47 +0530

    routing done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'medyfi';
+  constructor(private router: Router){
+
+  }
+  openhome(){
+    this.router.navigate(['home']);
+  };
+  openaboutus(){
+    this.router.navigate(['aboutus']);
+  };
+  opencareers(){
+    this.router.navigate(['careers']);
+  };
+  opencontactus(){
+    this.router.navigate(['contactus']);
+  ;}
+  openservices(){
+    this.router.navigate(['services']);
+  }
+
 }
